0a9651955e6fe854000d3e4ce3f305ad2876ed90
fedkh <khennoufadoua@gmail.com>
2019-06-25 00:51:00 +0100

    Delete twitter_credentials.py

--- twitter_credentials.py
+++
@@ -1,5 +0,0 @@
-# Variables that contains the user credentials to access Twitter API 
-ACCESS_TOKEN ="1119933248702906368-svque6qSQyjNCHocZrUzzHj07XFRtr"
-ACCESS_TOKEN_SECRET = "kPHMClggWwFN7fum1YWCq78ryEsWQZQFgj6brr6S2FFKp"
-CONSUMER_KEY = "aGEjf9JnFdgonhY6nDONXcqv5"
-CONSUMER_SECRET = "TXDXKLtzn23XcjBQvlQ7qKs1ezpUGCX6fgtacImwlpKxl7BFge"
